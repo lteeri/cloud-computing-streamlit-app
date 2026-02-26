b330a189491a92601f6efa6e5f7a3ce187cf36e0
lteeri <leiniteeri@gmail.com>
2026-02-26 19:42:34 +0200

    added the model to the main file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from model import buildModel
 
 st.title('Cloud Computing Module 3 Bonus Task')
+
+
+model = buildModel()
+# let's show to the user when the model is loaded
+st.write("Model loaded: ", bool(model))
