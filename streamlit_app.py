9e7657c1a8c82ff1868b186bbbfb62a25964969e
lteeri <leiniteeri@gmail.com>
2026-02-26 20:04:52 +0200

    added the form logit with the help of streamlit webpage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 from model import buildModel
 
 st.title('Cloud Computing Module 3 Bonus Task')
@@ -14,3 +15,20 @@ st.write("This tool will analyze whether your sentence is positive, " \
 
 # let's show to the user when the model is loaded
 st.write("Model loaded: ", bool(model))
+
+st.subheader('Sentiment analysis tool')
+if 'sentence' not in st.session_state:
+    st.session_state.sentence = "-"
+
+
+def analyse_sentence():
+    analysis = model.predict([st.session_state.input_sentence])
+    st.session_state.sentence = analysis
+
+
+with st.form(key='my_form'):
+
+    st.text_input('Enter your sentence', value="", placeholder="Sentence...", key="input_sentence")
+    submit = st.form_submit_button(label='Analyze', on_click=analyse_sentence)
+
+st.write(f'Analysis result = {st.session_state.sentence[0]}')
