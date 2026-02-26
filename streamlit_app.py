63cec83c98691bc53f280f4ccb1c4feba8476926
lteeri <leiniteeri@gmail.com>
2026-02-26 19:46:21 +0200

    added texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,12 @@ st.title('Cloud Computing Module 3 Bonus Task')
 
 
 model = buildModel()
+
+st.write("This tool was an assignment on my Cloud Computing course. " \
+"We trained our own sentiment analysis tool using Skikit learn Pyhton " \
+"libraries.")
+st.write("This tool will analyze whether your sentence is positive, " \
+"neutral or negative.")
+
 # let's show to the user when the model is loaded
 st.write("Model loaded: ", bool(model))
